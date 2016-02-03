72b1360ea93cc3e2516aee9eac45c8059b140c0e
akaocenterfield <akao@centerfield.com>
2016-02-03 15:27:47 -0800

    Update parse_bids.py
    update

--- parse_bids.py
+++ parse_bids.py
@@ -1,55 +1,63 @@
 import os 
+import re
 import pandas as pd
 import datetime
 
-df = pd.read_csv('change_history.csv', skiprows=range(0,1))
-df.columns.values
+df = pd.read_csv('ch.csv', skiprows=range(0,1))
 
 line_num = list()
 dates = list()
 YY = list()
 MM = list()
 DD = list()
+campaign = list()
+adgroup = list()
 keyword = list()
 before_bid = list()
 after_bid = list()
 
 for ii in range(0, len(df)):
+    cp = df['Campaign'][ii]
+    ag = df['Ad Group'][ii]
     str = df['Changes'][ii] 
-    str_list = str.split(' ',3)
-    if (str_list[1] != 'max' and str_list[2] != 'CPC'):
-        print ii, 'th row does not contain Max. CPC changes!'
-        continue
     date_time = datetime.datetime.strptime(df['Date & time'][ii], '%b %d, %Y %I:%M:%S %p')
     date_string = date_time.strftime('%Y-%m-%d')
-    num_kw = int(str_list[0])   
-    bid_change = str.split('\n') 
-    
-    for jj in range(0, num_kw):
-        tmp = bid_change[jj+1].split(':')
-        kw = tmp[0][3:-1]                
-        try:
-            before = float(tmp[1].split('$')[1].split(' ')[0])
-        except: 
-            before = float(tmp[1].split('$')[1].split(')')[0])
-        after = float(tmp[1].split('$')[2])      
-
-        line_num.append(ii)
-        dates.append(date_string)
-        YY.append(date_time.year) 
-        MM.append(date_time.month) 
-        DD.append(date_time.day)
-        keyword.append(kw)
-        before_bid.append(before)
-        after_bid.append(after) 
+    str_split = re.split('\n',str)
+	
+    for jj in range(0, len(str_split)):
+        split2 = re.split(' ',str_split[jj])
+		
+        if (split2[1] == 'Exact' and split2[4] == 'max' and split2[5] == 'CPC'):
+            num_kw = int(split2[0])	
+			
+            for kk in range(1, num_kw+1):
+                split3 = re.split('\$', str_split[jj + kk])
+                kw_indx1 = split3[0].index('[')
+                kw_indx2 = split3[0].index(']')
+                kw = split3[0][kw_indx1 + 1 : kw_indx2]
+                before = float(re.split(' ',split3[1])[0])
+                after = float(split3[2])
+                line_num.append(ii)
+                dates.append(date_string)
+                YY.append(date_time.year) 
+                MM.append(date_time.month) 
+                DD.append(date_time.day)
+                campaign.append(cp)
+                adgroup.append(ag)
+                keyword.append(kw)
+                before_bid.append(before)
+                after_bid.append(after) 
+   
 
 clean_df = pd.DataFrame({'Line No.': line_num})
 clean_df['Date'] = dates
 clean_df['Year'] = YY
 clean_df['Month'] = MM
 clean_df['Day'] = DD
+clean_df['Campaign'] = campaign
+clean_df['Adgroup'] = adgroup
 clean_df['Keyword'] = keyword
 clean_df['Before'] = before_bid
 clean_df['After'] = after_bid
-clean_df.to_csv('bids.csv', sep=',')
-
+clean_df.to_csv('cleanedup.csv', sep=',')
+       

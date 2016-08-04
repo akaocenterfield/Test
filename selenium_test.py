751f72f9995fe03e4d0a0249f0c5cd8edba835c7
akaocenterfield <akao@centerfield.com>
2016-08-04 10:26:25 -0700

    color

--- selenium_test.py
+++ selenium_test.py
@@ -2,63 +2,62 @@ from pytesseract import image_to_string
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import pandas as pd
 import time
 import re
 import os
 
 os.chdir('C:/')
 
-kw = ["flower online", "buy flowers online", "deliver flowers", "online flower delivery"]
+kw = []
 
 
-def find_yellow_pixels(filename):
+def find_green_pixels(filename):
     im = Image.open(filename)
     pix = im.load()
     x_max = im.size[0]
     y_max = im.size[1]
     for i in range(x_max):
         for j in range(y_max):
-            if pix[i, j] == (239, 196, 57):
-                yellow_pixel = 1
-                return yellow_pixel
+            if pix[i,j] == (89, 148, 107): # (239, 196, 57) yellow
+                green_pixel = 1
+                return green_pixel
 
 
-def filter_yellow_pixels(im):
+def invert_green_pixels(im):
     pix = im.load()
     x_max = im.size[0]
     y_max = im.size[1]
-    yellow_pix_x = list()
-    yellow_pix_y = list()
+    green_pix_x = list()
+    green_pix_y = list()
     for i in range(x_max):
         for j in range(y_max):
-            if pix[i,j] == (239, 196, 57):
-                yellow_pix_x.append(i)
-                yellow_pix_y.append(j)
+            if pix[i,j] == (89, 148, 107):
+                green_pix_x.append(i)
+                green_pix_y.append(j)
 
-    yellow_x = sorted(list(set(yellow_pix_x)))
-    yellow_y = sorted(list(set(yellow_pix_y)))
-    yellow_begin_x = [yellow_x[0]]
-    yellow_end_x = []
-    for i in range(len(yellow_x)-1):
-        diff = yellow_x[i+1] - yellow_x[i]
+    green_x = sorted(list(set(green_pix_x)))
+    green_y = sorted(list(set(green_pix_y)))
+    green_begin_x = [green_x[0]]
+    green_end_x = []
+    for i in range(len(green_x)-1):
+        diff = green_x[i+1] - green_x[i]
         if diff != 1:
-            yellow_begin_x.append(yellow_x[i+1])
-            yellow_end_x.append(yellow_x[i])
-    yellow_end_x.append(yellow_x[-1])
-
-    yellow_begin_y = [yellow_y[0]]
-    yellow_end_y = []
-    for i in range(len(yellow_y)-1):
-        diff = yellow_y[i+1] - yellow_y[i]
+            green_begin_x.append(green_x[i+1])
+            green_end_x.append(green_x[i])
+    green_end_x.append(green_x[-1])
+    green_begin_y = [green_y[0]]
+    green_end_y = []
+    for i in range(len(green_y)-1):
+        diff = green_y[i+1] - green_y[i]
         if diff != 1:
-            yellow_begin_y.append(yellow_y[i+1])
-            yellow_end_y.append(yellow_y[i])
-    yellow_end_y.append(yellow_y[-1])
-
-    for k in range(len(yellow_begin_y)):
-        for i in range(yellow_begin_x[0], yellow_end_x[0] + 1):
-            for j in range(yellow_begin_y[k], yellow_end_y[k] + 1):
-                if pix[i, j] == (239, 196, 57):
+            green_begin_y.append(green_y[i+1])
+            green_end_y.append(green_y[i])
+    green_end_y.append(green_y[-1])
+    for k in range(len(green_begin_y)):
+        for i in range(green_begin_x[0], green_end_x[0] + 1):
+            for j in range(green_begin_y[k], green_end_y[k] + 1):
+                if pix[i, j] == (89, 148, 107): #(239, 196, 57)
                     pix[i, j] = (255, 255, 255)
                 elif pix[i, j][0] > 240 & pix[i, j][1] > 240 & pix[i, j][1] < 200:
                     pix[i, j] = (255, 255, 255)
@@ -74,9 +73,8 @@ def tidy_url(url):
 
 
 def find_ad_urls(keyword):
-
     driver = webdriver.Chrome()
-    driver.get("http://ask.com")
+    driver.get("http://www.com")
     elem = driver.find_element_by_name("q")
     elem.send_keys(keyword)
     elem.send_keys(Keys.RETURN)
@@ -100,18 +98,18 @@ def find_ad_urls(keyword):
     ads_in_pic = []
     for i in range(n):
         string = keyword + str(i+1)
-        if find_yellow_pixels('%s.png' % string):
+        if find_green_pixels('%s.png' % string):
             im = Image.open('%s.png' % string)
-            im2 = filter_yellow_pixels(im)
+            im2 = invert_green_pixels(im)
             im2.save('%s_ch.png' % string)
             ads_in_pic.append(1)
         else:
-            #print 'no Ads!'
+            # print 'no Ads!'
             ads_in_pic.append(0)
-    #print ads_in_pic
+    # print ads_in_pic
 
     all_dict = {}
-    ad_urls = []
+    advertiser_urls = []
     p = 1
     for j in range(n):
         p = p * ads_in_pic[j]
@@ -125,22 +123,38 @@ def find_ad_urls(keyword):
             #print lines
             for i in range(0, len(lines)):
                 if "Ad" in lines[i][0:4]:
-                    sss = re.split(' ', lines[i])[1]
-                    clean_url = tidy_url(sss)
-                    if clean_url not in ad_urls:
-                        ad_urls.append(clean_url)
+                    raw_url = re.split(' ', lines[i])[1]
+                    clean_url = tidy_url(raw_url)
+                    count_dot = clean_url.count(".")
+                    count_slash = clean_url.count("/")
+                    if count_dot != 2:
+                        continue
+
+                    try:
+                        clean_url = clean_url[0:clean_url.index('/')]
+                    except:
+                        pass
+
+                    if clean_url not in advertiser_urls:
+                        advertiser_urls.append(clean_url)
                         if p == 1:
-                            rank = str(len(ad_urls)) + ' top'
-                            final_desc =  str(len(ad_urls)) + ' (top) : ' + clean_url
+                            rank = str(len(advertiser_urls)) + ' top'
+                            final_desc =  str(len(advertiser_urls)) + ' (top) : ' + clean_url
                         else:
-                            rank = str(len(ad_urls)) + ' bottom'
-                            final_desc =  str(len(ad_urls)) + ' (bottom) : ' + clean_url
+                            rank = str(len(advertiser_urls)) + ' bottom'
+                            final_desc =  str(len(advertiser_urls)) + ' (bottom) : ' + clean_url
                         if clean_url not in all_dict.keys():
                             all_dict[clean_url] = rank
                         #print final_desc
-    print all_dict
+    output_df = pd.DataFrame(all_dict, index=[keyword])
+    print output_df
+    return output_df
 
 
+results = pd.DataFrame()
 for q in range(len(kw)):
     print kw[q]
-    find_ad_urls(kw[q])
+    output = find_ad_urls(kw[q])
+    results = results.append(output)
+
+results.to_csv('results.csv')
